8225b74e4e6b19e05b804f18f556ef2e1f528406
drewbuensuceso <rayandrew.buensuceso@gmail.com>
2024-03-12 12:25:43 +0800

    added restframework in settings and modified auth_user_model

--- timetracker_rest_api/timetracker_rest_api/settings.py
+++ timetracker_rest_api/timetracker_rest_api/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'rest_framework.authtoken',
     'account',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -127,3 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
