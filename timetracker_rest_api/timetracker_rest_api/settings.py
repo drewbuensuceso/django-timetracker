c39244e1c988c8586389a5fa315891e848385ed3
drewbuensuceso <rayandrew.buensuceso@gmail.com>
2024-03-12 17:21:23 +0800

    added application for timetracker

--- timetracker_rest_api/timetracker_rest_api/settings.py
+++ timetracker_rest_api/timetracker_rest_api/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'account',
+    'timeentry',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
