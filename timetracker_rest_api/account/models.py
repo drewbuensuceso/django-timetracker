59663d227799b713dc3de737591151548f07bb41
drewbuensuceso <rayandrew.buensuceso@gmail.com>
2024-03-12 12:55:20 +0800

    modified CustomUser to return username

--- timetracker_rest_api/account/models.py
+++ timetracker_rest_api/account/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     # Add any additional fields you want for your user model
-    pass
+    def __str__(self):
+        return self.username
