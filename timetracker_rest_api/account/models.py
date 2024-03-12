1d614f4199095e75d86b6bb0e7e8ebb5f15bbbdb
drewbuensuceso <rayandrew.buensuceso@gmail.com>
2024-03-12 12:24:58 +0800

    added CustomerUser model

--- timetracker_rest_api/account/models.py
+++ timetracker_rest_api/account/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Add any additional fields you want for your user model
+    pass
